33ef07c3a4d7e5249d77eb1a863f35fe751ebc63
Islam Ibrakhimzhanov <36242677+Ibrakhimzhanov@users.noreply.github.com>
2026-01-23 08:56:42 +0500

    feat: добавлен GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://ibrakhimzhanov.github.io',
+  base: '/brightfuture',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'uz',
